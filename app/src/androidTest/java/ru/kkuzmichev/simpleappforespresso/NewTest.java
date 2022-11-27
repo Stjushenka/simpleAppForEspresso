95acb310fe4f204d1acecc09abf6ba7384fff3b3
Stjushenka <stjushenka@gmail.com>
2022-11-27 20:49:33 +0300

    TestReformate

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/NewTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/NewTest.java
@@ -1,37 +1,32 @@
 package ru.kkuzmichev.simpleappforespresso;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
+import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static org.hamcrest.Matchers.allOf;
-
-import static kotlin.jvm.internal.Intrinsics.checkNotNull;
-
-import android.content.Intent;
-import android.webkit.WebView;
-
-
-import androidx.test.espresso.IdlingRegistry;
+import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.contrib.DrawerActions;
 import androidx.test.espresso.contrib.NavigationViewActions;
-import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import android.content.Intent;
 
-import org.junit.After;
-import org.junit.Before;
+import static org.hamcrest.Matchers.allOf;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.After;
+import org.junit.Before;
 
 import ru.kkuzmichev.simpleappforespresso.ui.EspressoIdlingResources;
 
@@ -54,29 +49,22 @@ public class NewTest {
     }
 
     @Test
-    public void Intent() {
-        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
-        ViewInteraction settingsInteraction = onView(withText("Settings"));
-        settingsInteraction.check(matches(isDisplayed()));
-        settingsInteraction.perform(click());
-        ViewInteraction chromeInteraction =onView(withText("Chrome"));
+    public void intent() {
+        ViewInteraction settingsButton = onView(withContentDescription("More options"));
+        ViewInteraction settingsTextView = onView(withId(R.id.title));
 
-        //Intents... Если вы решили реализовать проверку с подписыванием на Intent
-        chromeInteraction.perform(click()); //Для запуска intent
-        Intents.intended(allOf(hasData("https://google.com"), hasAction(Intent.ACTION_VIEW)));//Проверяем intent, он должен передавать url и action
-        //Intents..
+        settingsButton.check(matches(isDisplayed()));
+        settingsButton.perform(click());
+
+        Intents.init();
+        settingsTextView.perform(click());
+        intended(allOf(hasData("https://google.com"),
+                hasAction(Intent.ACTION_VIEW)));
+        Intents.release();
     }
 
 
-    @Before
-    public void beforeIdleTest() {
-        IdlingRegistry.getInstance().register(EspressoIdlingResources.idling_resource);
-    }
 
-    @After
-    public void afterIdleTest() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idling_resource);
-    }
 
     @Test
     public void idleListTest() {
